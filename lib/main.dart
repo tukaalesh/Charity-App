9e1b6ff7032fd1ae5bea5ccf7a1ee834dc923a8a
tuka <tukaalesh8@gmail.com>
2025-06-30 22:20:18 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:charity_app/auth/cubits/auth_cubits/auth_cubits.dart';
 import 'package:charity_app/auth/cubits/change_password/change_password_cubit.dart';
+import 'package:charity_app/auth/cubits/pin_code_cubit/pin_code_cubit.dart';
 import 'package:charity_app/auth/cubits/splash_cubits/splash_cubits.dart';
 import 'package:charity_app/auth/screens/change_password_screen.dart';
 import 'package:charity_app/auth/screens/login_screen.dart';
@@ -10,15 +11,30 @@ import 'package:charity_app/auth/screens/welcome_screen.dart';
 import 'package:charity_app/core/theme/app_themes.dart';
 import 'package:charity_app/feature/gift/cubit/send_gift_cubit.dart';
 import 'package:charity_app/feature/gift/screen/gift_screen.dart';
+import 'package:charity_app/feature/monthly_donation/cubit/cancle_monthly_donation.dart';
+import 'package:charity_app/feature/monthly_donation/cubit/monthly_donation_cubit.dart';
+import 'package:charity_app/feature/monthly_donation/screens/enable_monthly_onation.dart';
+import 'package:charity_app/feature/monthly_donation/screens/monthly_donation_screen.dart';
 import 'package:charity_app/feature/notification/screen/notification_screen.dart';
-import 'package:charity_app/feature/voluntary/cubit/cubit_request/voluntary_cubits.dart';
-import 'package:charity_app/feature/voluntary/screen/voluntary_screen.dart';
-import 'package:charity_app/feature/voluntary/screen/volunteering_fields.dart';
+import 'package:charity_app/feature/volunteer%20projects/screens/fields.dart';
+import 'package:charity_app/feature/volunteer%20projects/screens/globalNetwork_projects.dart';
+import 'package:charity_app/feature/volunteer%20projects/screens/healtcare_projects.dart';
+import 'package:charity_app/feature/volunteer%20projects/screens/learning_projects_screen.dart';
+import 'package:charity_app/feature/volunteer%20projects/screens/on_site_projects.dart';
+import 'package:charity_app/feature/volunteer%20request/cubit/cubit_request/voluntary_cubits.dart';
+import 'package:charity_app/feature/volunteer%20request/screen/voluntary_screen.dart';
 import 'package:charity_app/feature/wallet/cubit/wallet_cubit.dart';
 import 'package:charity_app/feature/wallet/screen/wallet_screen.dart';
-import 'package:charity_app/home/cubit/theme_cubit/theme_cubits.dart';
-import 'package:charity_app/home/screens/home_screen.dart';
-import 'package:charity_app/home/screens/setting_screen.dart';
+import 'package:charity_app/home/cubits/donation_repositry.dart';
+import 'package:charity_app/home/cubits/navigation/navigation_cubit.dart';
+import 'package:charity_app/home/cubits/project_cubit/donation_cubit.dart';
+import 'package:charity_app/home/cubits/themeCubit/theme_cubit.dart';
+import 'package:charity_app/home/cubits/user_cubit/user_cubit.dart';
+import 'package:charity_app/home/screens/home_page.dart';
+import 'package:charity_app/home/screens/navigation_main.dart';
+import 'package:charity_app/home/screens/setting_drawer.dart';
+import 'package:charity_app/zakat/cubit/zakah_cubit.dart';
+import 'package:charity_app/zakat/screen/zakah_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -30,8 +46,7 @@ final token = sharedPreferences.get("token");
 //مشان مانموت ونحنا عم نعدل بين ايميوليتر و ويندوز
 const String localhost = "127.0.0.1:8000";
 
-// ignore: unused_local_variable, prefer_const_declarations
-// String localhost = "10.0.2.2:8000";
+//const String localhost = "10.0.2.2:8000";
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   sharedPreferences = await SharedPreferences.getInstance();
@@ -55,7 +70,14 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (context) => ChangePasswordCubit()),
         BlocProvider(create: (context) => WalletCubit()),
         BlocProvider(create: (context) => SendGiftCubit()),
-        BlocProvider(create: (context) => VoluntaryCubits())
+        BlocProvider(create: (context) => VoluntaryCubits()),
+        BlocProvider(create: (context) => DonationCubit(DonationRepositry())),
+        BlocProvider(create: (context) => NavigationCubit()),
+        BlocProvider(create: (context) => PinCodeCubit()),
+        BlocProvider(create: (context) => MonthlyDonationCubit()),
+        BlocProvider(create: (context) => ZakahCubit()),
+        BlocProvider(create: (context) => UserCubit()),
+        BlocProvider(create: (context) => CancleMonthlyDonationCubit())
       ],
       child: BlocBuilder<ThemeCubits, bool>(
         builder: (context, isDarkMode) {
@@ -76,7 +98,8 @@ class MyApp extends StatelessWidget {
             initialRoute: "Splash",
             routes: {
               "Splash": (context) => const SplashScreen(),
-              "Home": (context) => const HomeScreen(),
+              "NavigationMain": (context) => const NavigationMain(),
+              "Home": (context) => const HomePage(),
               "SignUp": (context) => SignUpScreen(),
               "LogIn": (context) => LoginScreen(),
               "Welcom": (context) => const WelcomeScreen(),
@@ -88,6 +111,14 @@ class MyApp extends StatelessWidget {
               "Wallet": (context) => WalletScreen(),
               "Notification": (context) => const NotificationScreen(),
               "VolunteeringFields": (context) => const VolunteeringFields(),
+              "LearningProject": (context) => const LearningProject(),
+              "OnSiteProjects": (context) => const OnSiteProjects(),
+              "GlobalnetworkProjects": (context) =>
+                  const GlobalnetworkProjects(),
+              "HealtcareProjects": (context) => const HealtcareProjects(),
+              "ZakahPage": (context) => const ZakahPage(),
+              "MonthlyDonation": (context) => const MonthlyDonationScreen(),
+              "EnableMonthlyOnation": (context) => EnableMonthlyOnation(),
             },
           );
         },
